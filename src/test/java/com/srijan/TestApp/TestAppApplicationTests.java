803626998125f427783d64bddf269017bf8cb992
Sr1jan27 <singhsrijan809@gmail.com>
2024-09-24 11:00:55 +0530

    first Commit

---
+++ src/test/java/com/srijan/TestApp/TestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srijan.TestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
